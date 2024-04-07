cdc63785be9cba191158d1f0bb9fc589b78456a7
Jonhon <jonhon.lenon@stand.com.br>
2024-04-07 17:05:47 -0300

    Criação do projeto

---
+++ Api/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.UseHttpsRedirection();
+app.Run();
