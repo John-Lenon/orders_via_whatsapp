6c504f18a3e6297ad647bc40d329ead79c17eb7f
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 22:59:22 -0300

    Criando interfaces dos repositorios Base e Usuario, e implementando metodo de adiconar permissão.

---
+++ src/03 - Core/Domain/Interfaces/Usuario_/IUsuarioRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.Usuario;
+using Domain.Enumeradores.Pemissoes;
+using Domain.Interfaces.Base;
+
+namespace Domain.Interfaces.Usuario_
+{
+    public interface IUsuarioRepositorio : IRepositoryBase<Usuario>
+    {
+        Task AdicionarPermissaoAoUsuarioAsync(int usuarioId, params EnumPermissoes[] permissoes);
+    }
+}
