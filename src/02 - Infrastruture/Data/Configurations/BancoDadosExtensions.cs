162e6e166ab28dabde40f3c5121a4976f359f6f0
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 23:18:43 -0300

    Criando Usuario inicial com todas as permissoes

--- src/02 - Infrastruture/Data/Configurations/BancoDadosExtensions.cs
+++ src/02 - Infrastruture/Data/Configurations/BancoDadosExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Application.Configurations;
-using Data.Context;
+﻿using Data.Context;
+using Domain.Entities.Usuario;
+using Domain.Enumeradores.Pemissoes;
+using Domain.Interfaces.Usuario_;
+using Domain.Utilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,26 +10,62 @@ namespace Data.Configurations
 {
     public static class BancoDadosExtensions
     {
-        public static IServiceProvider ConfigurarBancoDeDados(this IServiceProvider serviceProvider, CompanyConnectionStrings companies)        
+        public static void ConfigurarBancoDeDados(
+            this IServiceProvider serviceProvider,
+            CompanyConnectionStrings companies
+        )
         {
             using var serviceScope = serviceProvider.CreateScope();
-            var dbContext = serviceScope.ServiceProvider.GetRequiredService<OrderViaWhatsAppContext>();
+            var service = serviceScope.ServiceProvider;
+
+            var dbContext = service.GetRequiredService<OrderViaWhatsAppContext>();
 
             foreach (var company in companies.List)
             {
                 dbContext.Database.SetConnectionString(company.ConnnectionString);
                 dbContext.Database.Migrate();
             }
-            return serviceProvider;
+
+            PrepareUserAdmin(service);
         }
 
-        public static IServiceCollection ConfigurarBancoDeDados(this IServiceCollection serviceCollection)
+        public static void ConfigurarBancoDeDados(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddDbContext<OrderViaWhatsAppContext>(options =>
             {
                 options.UseSqlServer();
             });
-            return serviceCollection;
+        }
+
+        public static void PrepareUserAdmin(IServiceProvider service)
+        {
+            var usuarioRepository = service.GetRequiredService<IUsuarioRepositorio>();
+
+            string email = "master@gmail.com";
+            string senha = "Master@123456";
+
+            if (usuarioRepository.Get(u => u.Email == email).FirstOrDefault() != null)
+                return;
+
+            var (codigoUnicoSenha, senhaHash) = PasswordHasher.GerarSenhaHash(senha);
+
+            var usuario = new Usuario
+            {
+                Nome = "Master",
+                Email = email,
+                Telefone = "(31) 99999-9999",
+                SenhaHash = senhaHash,
+                CodigoUnicoSenha = codigoUnicoSenha,
+                Permissoes = []
+            };
+
+            var permissoes = new EnumPermissoes[] { EnumPermissoes.USU_000001 };
+
+            usuarioRepository.InsertAsync(usuario).Wait();
+            if (!usuarioRepository.SaveChangesAsync().Result)
+                return;
+
+            usuarioRepository.AdicionarPermissaoAoUsuarioAsync(usuario.Id, permissoes).Wait();
         }
     }
 }
