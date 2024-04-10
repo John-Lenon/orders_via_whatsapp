c66240fd0d8a201a7be04f1d36542288b9ad7c76
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 23:05:18 -0300

    criando classes de extensao para gerencias as dependencias da aplicação

---
+++ src/03 - Core/Application/Configurations/ManageDependencies/DependencyInjectionsApplication.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces.Auth;
+using Application.Services.Auth;
+using Application.Utilities;
+using Data.Repository.Usuario_;
+using Domain.Interfaces.Usuario_;
+using Domain.Interfaces.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Configurations.ManageDependencies
+{
+    public static class DependencyInjectionsApplication
+    {
+        public static void AddAllDependencyInjections(this IServiceCollection services)
+        {
+            services.AddDependecyUtilities();
+            services.AddDependecyRepositories();
+            services.AddDependecyServices();
+        }
+
+        public static void AddDependecyUtilities(this IServiceCollection services)
+        {
+            services.AddScoped<INotificador, Notificador>();
+        }
+
+        public static void AddDependecyRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+        }
+
+        public static void AddDependecyServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+        }
+    }
+}
