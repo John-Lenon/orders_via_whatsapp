51b5aa0fc6f092d03e09b40e080d0d634d0514e2
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 22:56:41 -0300

    Criando DTOs

---
+++ src/03 - Core/Domain/DTOs/Usuario/UsuarioDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.DTOs.Usuario
+{
+    public class UsuarioDto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Senha { get; set; }
+    }
+}
