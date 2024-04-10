e73a49bc57bd1f7bc77bc763ed7310f353f17b10
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 23:22:53 -0300

    criando atributos para validar permissões e autenticação

---
+++ src/01 - Presentation/Api/Attributes/AutorizationApi.cs
@@ -0,0 +1,32 @@
+﻿using Api.Base;
+using Domain.Enumeradores.Notificacao;
+using Domain.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AutorizationApi : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var response = new ResponseResultDTO<string>()
+                {
+                    Mensagens =
+                    [
+                        new Notificacao(
+                            EnumTipoNotificacao.Erro,
+                            "Acesso não autorizado. Você precisa estar autenticado."
+                        )
+                    ]
+                };
+
+                context.Result = new ObjectResult(response) { StatusCode = 401 };
+                return;
+            }
+        }
+    }
+}
---
+++ src/01 - Presentation/Api/Attributes/PermissoesApi.cs
@@ -0,0 +1,42 @@
+﻿using Api.Base;
+using Domain.Enumeradores.Notificacao;
+using Domain.Enumeradores.Pemissoes;
+using Domain.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class PermissoesApi(params EnumPermissoes[] enumPermissoes)
+        : Attribute,
+            IAuthorizationFilter
+    {
+        private IEnumerable<string> EnumPermissoes { get; } =
+            enumPermissoes.Select(x => x.ToString());
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var possuiTodasPermissoes = EnumPermissoes.All(permissao =>
+                context.HttpContext.User.Claims.Any(claim => claim.Value == permissao)
+            );
+
+            if (!possuiTodasPermissoes)
+            {
+                var response = new ResponseResultDTO<string>()
+                {
+                    Mensagens =
+                    [
+                        new Notificacao(
+                            EnumTipoNotificacao.Erro,
+                            "Você não tem permissão para acessar esse recurso."
+                        )
+                    ]
+                };
+
+                context.Result = new ObjectResult(response) { StatusCode = 401 };
+                return;
+            }
+        }
+    }
+}
