ffb6eb8697751418914e586c61252d6315d548f6
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 22:59:49 -0300

    Criando Enum para permissão

---
+++ src/03 - Core/Domain/Enumeradores/Pemissoes/EnumPermissoes.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Domain.Enumeradores.Pemissoes
+{
+    public enum EnumPermissoes
+    {
+        /// <summary>
+        /// Permissao para teste.
+        /// </summary>
+        [Description("Permissao para teste.")]
+        USU_000001 = 1,
+    }
+}
