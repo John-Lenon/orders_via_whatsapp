9f3d8d622fbd914c518566c0b68e6ebb16e75774
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 23:06:26 -0300

    Criando repositorio  Base e implementando repositorio de Usuario.

---
+++ src/02 - Infrastruture/Data/Repository/Usuario_/UsuarioRepositorio.cs
@@ -0,0 +1,39 @@
+﻿using Data.Context;
+using Data.Repository.Base;
+using Domain.Entities.Usuario;
+using Domain.Enumeradores.Pemissoes;
+using Domain.Interfaces.Usuario_;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repository.Usuario_
+{
+    public class UsuarioRepositorio(IServiceProvider service)
+        : RepositorioBase<Usuario, OrderViaWhatsAppContext>(service),
+            IUsuarioRepositorio
+    {
+        public async Task AdicionarPermissaoAoUsuarioAsync(
+            int usuarioId,
+            params EnumPermissoes[] permissoes
+        )
+        {
+            var usuario = await Get(user => user.Id == usuarioId)
+                .Include(p => p.Permissoes)
+                .FirstOrDefaultAsync();
+
+            foreach (var permissao in permissoes)
+            {
+                var possuiPermissao = usuario
+                    .Permissoes.Where(p => p.Descricao == permissao.ToString())
+                    .FirstOrDefault();
+
+                if (possuiPermissao is null)
+                {
+                    usuario.Permissoes.Add(new Permissao { Descricao = permissao.ToString() });
+                }
+            }
+
+            Update(usuario);
+            await SaveChangesAsync();
+        }
+    }
+}
