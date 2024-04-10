a9769b79b9be985b955d823852b1846b352cae8f
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 22:57:51 -0300

    Criando classe de suporte para transforma senha em Hash

---
+++ src/03 - Core/Domain/Utilities/PasswordHasher.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace Domain.Utilities
+{
+    public class PasswordHasher
+    {
+        public static (string Salt, string Hash) GerarSenhaHash(string senha)
+        {
+            byte[] codigoUnicoSenha = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(codigoUnicoSenha);
+            }
+
+            string hash = Convert.ToBase64String(
+                KeyDerivation.Pbkdf2(
+                    password: senha,
+                    salt: codigoUnicoSenha,
+                    prf: KeyDerivationPrf.HMACSHA256,
+                    iterationCount: 10000,
+                    numBytesRequested: 48
+                )
+            );
+
+            return (Convert.ToBase64String(codigoUnicoSenha), hash);
+        }
+
+        public static string CompararSenhaHash(string senha, string codigoUnicoSenha)
+        {
+            byte[] codigoUnicoSenhaBytes = Convert.FromBase64String(codigoUnicoSenha);
+
+            return Convert.ToBase64String(
+                KeyDerivation.Pbkdf2(
+                    password: senha,
+                    salt: codigoUnicoSenhaBytes,
+                    prf: KeyDerivationPrf.HMACSHA256,
+                    iterationCount: 10000,
+                    numBytesRequested: 48
+                )
+            );
+        }
+    }
+}
