cade7a106ff9519296e7adf327b7e36163c61126
Denis Developer <denispereiradosreis@gmail.com>
2024-04-09 23:03:25 -0300

    interface para AuthService

---
+++ src/03 - Core/Application/Interfaces/Auth/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.DTOs.Usuario;
+using Domain.Enumeradores.Pemissoes;
+
+namespace Application.Interfaces.Auth
+{
+    public interface IAuthService
+    {
+        Task<UsuarioTokenDto> AutenticarUsuario(UsuarioDto userDto);
+        Task<UsuarioTokenDto> CadastrarUsuario(UsuarioDto userDto);
+        bool PossuiPermissao(params EnumPermissoes[] permissoesParaValidar);
+    }
+}
